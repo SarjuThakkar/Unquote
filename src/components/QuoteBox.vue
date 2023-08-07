b97e47b7a64975a505a80ea022deddf012b5637a
Sarju Thakkar <thakkar.96@osu.edu>
2023-08-06 22:06:38 -0500

    made text smaller

--- src/components/QuoteBox.vue
+++ src/components/QuoteBox.vue
@@ -8,7 +8,7 @@
     </div>
     <div class="mx-auto max-w-2xl py-8 sm:py-12 lg:py-14">
       <div class="text-center">
-        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">"{{ quote }}"</h1>
+        <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">"{{ quote }}"</h1>
         <p v-if="reveal" class="mt-6 text-lg leading-8 text-gray-600">{{ title }} by {{ author }}</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <button v-if="!reveal" @click="reveal = true"
